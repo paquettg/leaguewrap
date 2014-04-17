079ecb48015cac8bbfb4a633b88f00182ed036a3
Gilles Paquette <paquettg@gmail.com>
2014-04-17 11:48:28 -0400

    Added tests for the asia and eu subdomains

--- tests/Api/ChampionTest.php
+++ tests/Api/ChampionTest.php
@@ -65,5 +65,43 @@ class ApiChampionTest extends PHPUnit_Framework_TestCase {
 		$kayle = $api->champion()->championById(10);
 		$this->assertEquals(true, $kayle->rankedPlayEnabled);
 	}
+
+	public function testAllRegionKR()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once()
+		             ->with('https://asia.api.pvp.net/api/lol/');
+		$this->client->shouldReceive('request')
+		             ->with('kr/v1.2/champion', [
+						'freeToPlay' => 'false',
+						'api_key'    => 'key',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/champion.kr.json'));
+
+		$api = new Api('key', $this->client);
+		$api->setRegion('kr');
+		$champion = $api->champion();
+		$champion->all();
+		$this->assertTrue($champion->get(53) instanceof LeagueWrap\Dto\Champion);
+	}
+
+	public function testAllRegionRU()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once()
+		             ->with('https://eu.api.pvp.net/api/lol/');
+		$this->client->shouldReceive('request')
+		             ->with('ru/v1.2/champion', [
+						'freeToPlay' => 'false',
+						'api_key'    => 'key',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/champion.ru.json'));
+
+		$api = new Api('key', $this->client);
+		$api->setRegion('RU');
+		$champion = $api->champion();
+		$champion->all();
+		$this->assertTrue($champion->get(53) instanceof LeagueWrap\Dto\Champion);
+	}
 }
 
