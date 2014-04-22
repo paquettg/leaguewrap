30d14b7cc2bc760bebef737c32f75fdc3f93eb39
Gilles Paquette <paquettg@gmail.com>
2014-04-22 15:16:52 -0400

    Added tests for the static item calls

---
+++ tests/Api/Static/ItemTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use LeagueWrap\Api;
+use Mockery as m;
+
+class ItemTest extends PHPUnit_Framework_TestCase {
+	
+	protected $client;
+
+	public function setUp()
+	{
+		$client       = m::mock('LeagueWrap\Client');
+		$this->client = $client;
+	}
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	public function testGetItemDefault()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once();
+		$this->client->shouldReceive('request')
+		             ->with('na/v1.2/item', [
+						'api_key' => 'key',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/Static/items.json'));
+
+		$api   = new Api('key', $this->client);
+		$items = $api->staticData()->getItems();
+		$item  = $items->getItem(1001);
+		$this->assertEquals('Boots of Speed', $item->name);
+	}
+
+	public function testGetItemRegionKR()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once();
+		$this->client->shouldReceive('request')
+		             ->with('na/v1.2/item', [
+						'api_key' => 'key',
+						'locale'  => 'ko_KR',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/Static/items.kr.json'));
+
+		$api   = new Api('key', $this->client);
+		$items = $api->staticData()->setLocale('ko_KR')
+		                           ->getItems();
+		$item = $items->getItem(1042);
+		$this->assertEquals('단검', $item->name);
+	}
+
+	public function testGetItemById()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once();
+		$this->client->shouldReceive('request')
+		             ->with('na/v1.2/item/1051', [
+						'api_key' => 'key',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/Static/item.1051.json'));
+
+		$api   = new Api('key', $this->client);
+		$item = $api->staticData()->getItem(1051);
+		$this->assertEquals('Brawler\'s Gloves', $item->name);
+	}
+
+	public function testGetItemAll()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once();
+		$this->client->shouldReceive('request')
+		             ->with('na/v1.2/item', [
+						'api_key'      => 'key',
+						'itemListData' => 'all',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/Static/items.all.json'));
+
+		$api   = new Api('key', $this->client);
+		$items = $api->staticData()->getItems('all');
+		$item  = $items->getItem(1042);
+		$this->assertEquals(0.12, $item->stats->PercentAttackSpeedMod);
+	}
+}
