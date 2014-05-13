3d3c0e934eb8c17c8176db5a4ebecea8e6012f94
Gilles Paquette <paquettg@gmail.com>
2014-05-13 11:00:00 -0400

    Added tests for the remaining static calls

---
+++ tests/Api/Static/RuneTest.php
@@ -0,0 +1,86 @@
+<?php
+
+use LeagueWrap\Api;
+use Mockery as m;
+
+class RuneTest extends PHPUnit_Framework_TestCase {
+
+	protected $client;
+
+	public function setUp()
+	{
+		$client       = m::mock('LeagueWrap\Client');
+		$this->client = $client;
+	}
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	public function testGetRuneDefault()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once();
+		$this->client->shouldReceive('request')
+		             ->with('na/v1.2/rune', [
+						'api_key'  => 'key',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/Static/rune.json'));
+
+		$api   = new Api('key', $this->client);
+		$runes = $api->staticData()->getRunes();
+		$rune  = $runes->getRune(5129);
+		$this->assertEquals('Mark of Critical Chance', $rune->name);
+	}
+
+	public function testGetRuneRegionKR()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once();
+		$this->client->shouldReceive('request')
+		             ->with('kr/v1.2/rune', [
+						'api_key'  => 'key',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/Static/rune.kr.json'));
+
+		$api   = new Api('key', $this->client);
+		$runes = $api->setRegion('kr')
+		             ->staticData()->getRunes();
+		$rune  = $runes->getRune(5267);
+		$this->assertEquals('상급 주문력 표식', $rune->name);
+	}
+
+	public function testGetRuneById()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once();
+		$this->client->shouldReceive('request')
+		             ->with('na/v1.2/rune/5267', [
+						'api_key'  => 'key',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/Static/rune.5267.json'));
+
+		$api  = new Api('key', $this->client);
+		$rune = $api->staticData()->getRune(5267);
+		$this->assertEquals('3', $rune->rune->tier);
+	}
+
+	public function testGetRuneAll()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once();
+		$this->client->shouldReceive('request')
+		             ->with('na/v1.2/rune', [
+						'api_key'      => 'key',
+						'runeListData' => 'all',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/Static/rune.all.json'));
+
+		$api   = new Api('key', $this->client);
+		$runes = $api->staticData()->getRunes('all');
+		$rune  = $runes->getRune(5001);
+		$this->assertEquals('0.525', $rune->stats->FlatPhysicalDamageMod);
+	}
+}
+
---
+++ tests/Api/Static/SummonerSpellTest.php
@@ -0,0 +1,88 @@
+<?php
+
+use LeagueWrap\Api;
+use Mockery as m;
+
+class SummonerSpellTest extends PHPUnit_Framework_TestCase {
+
+	protected $client;
+
+	public function setUp()
+	{
+		$client       = m::mock('LeagueWrap\Client');
+		$this->client = $client;
+	}
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	public function testGetSummonerSpellDefault()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once();
+		$this->client->shouldReceive('request')
+		             ->with('na/v1.2/summoner-spell', [
+						'api_key'  => 'key',
+						'dataById' => 'true',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/Static/summonerspell.json'));
+		             
+		$api    = new Api('key', $this->client);
+		$spells = $api->staticData()->getSummonerSpells();
+		$spell  = $spells->getSpell(12);
+		$this->assertEquals('Teleport', $spell->name);
+	}
+
+	public function testGetSummonerSpellRegionTR()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once();
+		$this->client->shouldReceive('request')
+		             ->with('tr/v1.2/summoner-spell', [
+						'api_key'  => 'key',
+						'dataById' => 'true',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/Static/summonerspell.tr.json'));
+		             
+		$api    = new Api('key', $this->client);
+		$spells = $api->setRegion('tr')
+		              ->staticData()->getSummonerSpells();
+		$spell  = $spells->getSpell(6);
+		$this->assertEquals('Hayalet', $spell->name);
+	}
+
+	public function testGetSummonerSpellById()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once();
+		$this->client->shouldReceive('request')
+		             ->with('na/v1.2/summoner-spell/1', [
+						'api_key'  => 'key',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/Static/summonerspell.1.json'));
+		             
+		$api   = new Api('key', $this->client);
+		$spell = $api->staticData()->getSummonerSpell(1);
+		$this->assertEquals('2', $spell->summonerLevel);
+	}
+
+	public function testGetSummonerSpellAll()
+	{
+		$this->client->shouldReceive('baseUrl')
+		             ->once();
+		$this->client->shouldReceive('request')
+		             ->with('na/v1.2/summoner-spell', [
+						'api_key'   => 'key',
+						'dataById'  => 'true',
+						'spellData' => 'all',
+		             ])->once()
+		             ->andReturn(file_get_contents('tests/Json/Static/summonerspell.all.json'));
+		             
+		$api    = new Api('key', $this->client);
+		$spells = $api->staticData()->getSummonerSpells('all');
+		$spell  = $spells->getSpell(7);
+		$this->assertEquals('f1', $spell->vars[0]->key);
+	}
+}
