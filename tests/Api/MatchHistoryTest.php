06955b17aa6128ca1f4668e82a4882ce415fc38e
danijoo <danijob88@googlemail.com>
2014-08-19 23:39:24 +0200

    timeline test

--- tests/Api/MatchHistoryTest.php
+++ tests/Api/MatchHistoryTest.php
@@ -98,24 +98,21 @@ class ApiMatchHistoryTest extends PHPUnit_Framework_TestCase {
         $api   = new Api('key', $this->client);
         $matches = $api->matchHistory()->history(74602);
         $this->assertEquals(17, $matches->match(0)->participant(0)->stats->champLevel);
-//        $this->client->shouldReceive('baseUrl')
-//            ->twice();
-//        $this->client->shouldReceive('request')
-//            ->with('na/v1.3/game/by-summoner/74602/recent', [
-//                'api_key' => 'key',
-//            ])->once()
-//            ->andReturn(file_get_contents('tests/Json/game.recent.74602.json'));
-//        $this->client->shouldReceive('request')
-//            ->with('na/v1.4/summoner/by-name/bakasan', [
-//                'api_key' => 'key',
-//            ])->once()
-//            ->andReturn(file_get_contents('tests/Json/summoner.bakasan.json'));
-//
-//        $api     = new Api('key', $this->client);
-//        $bakasan = $api->summoner()->info('bakasan');
-//        $games   = $api->game()->recent($bakasan);
-//        $game    = $bakasan->recentGame(0);
-//        $this->assertEquals(13, $game->stats->level);
+    }
+
+    public function testParticipantTimeline()
+    {
+        $this->client->shouldReceive('baseUrl')
+            ->once();
+        $this->client->shouldReceive('request')
+            ->with('na/v2.2/matchhistory/74602', [
+                'api_key' => 'key',
+            ])->once()
+            ->andReturn(file_get_contents('tests/Json/matchhistory.74602.json'));
+
+        $api   = new Api('key', $this->client);
+        $matches = $api->matchHistory()->history(74602);
+        $this->assertEquals("BOTTOM", $matches->match(0)->participant(0)->timeline->lane);
     }
 
     public function testParticipantIdentity()
